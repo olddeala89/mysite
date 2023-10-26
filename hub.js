3ad1d28b512799198873df7632d9c655eebc62fb
olddeala <113938189+olddeala89@users.noreply.github.com>
2023-10-26 22:30:46 +0300

    Add files via upload

---
+++ hub.js
@@ -0,0 +1,16 @@
+let el = document.getElementById('git');
+let git = document.getElementById('first');
+let hub = document.getElementById('second');
+
+el.addEventListener('mouseover', pornhub);
+el.addEventListener('mouseout', cumback);
+
+function pornhub() {
+    git.classList.add('blak');
+    hub.classList.add('orng');
+}
+
+function cumback() {
+    git.classList.remove('blak');
+    hub.classList.remove('orng');
+}
